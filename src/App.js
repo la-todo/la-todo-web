23f546197d2ce93d1ec0586581fa797124d23b49
Kirill Vishenkov <vishkir1991@gmail.com>
2018-05-21 00:00:41 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">LA-todo</h1>
+        </header>
+        <p className="App-intro">
+          Basic setup for new app
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
